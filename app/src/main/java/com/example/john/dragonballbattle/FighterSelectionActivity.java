2cbb9e86cb0badabf704a80f7a104a25616fb710
John Marmo <jmbrba2445@gmail.com>
2018-04-25 18:58:00 -0400

    Updated the Model classes, added two new activities (create account and character selection). Added strings to the string folder

---
+++ app/src/main/java/com/example/john/dragonballbattle/FighterSelectionActivity.java
@@ -0,0 +1,13 @@
+package com.example.john.dragonballbattle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FighterSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fighter_selection);
+    }
+}
--- app/src/main/java/com/example/john/dragonballbattle/LoginActivity.java
+++ app/src/main/java/com/example/john/dragonballbattle/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.john.dragonballbattle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,4 +12,19 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void login(View v){
+        //TODO 4. VALIDATE login credentials in DB.
+        Intent completeIntent = new Intent( this, FighterSelectionActivity.class);
+        this.startActivity( completeIntent );
+        //return true;
+    }
+
+    public void createNewUser(View v){
+        Intent completeIntent = new Intent( this, CreateAccountActivity.class);
+        this.startActivity( completeIntent );
+       // return true;
+    }
+
+
 }
